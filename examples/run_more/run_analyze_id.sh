98e8e01134a6948e38f379285a986d1bc806d5ce
Fadli Aulawi <77540198+fadlia68@users.noreply.github.com>
2022-09-19 20:54:10 +0700

    Update run_analyze_id.sh

--- examples/run_more/run_analyze_id.sh
+++ examples/run_more/run_analyze_id.sh
@@ -1,9 +1,7 @@
 model_name="network.pt"
-cur_lang="id"
+cur_lang="en"
+which_set="test"
 
-python2 ../src/examples/analyze.py --parser biaffine --ordered \
- --punctuation 'PUNCT' 'SYM' \
- --out_filename analyzer.$cur_lang.out \
- --model_name $model_name \
- --test "../data2.2_more/jv_csui-ud-test.conllu" --model_path "./model/" \
- --extra_embed "../data2.2_more/wiki.multi.${cur_lang}.vec"
+PYTHONPATH=../src/ CUDA_VISIBLE_DEVICES=$RGPU python2 ../src/examples/analyze.py --parser biaffine --ordered --gpu \
+--punctuation 'PUNCT' 'SYM' --out_filename analyzer.$cur_lang.out --model_name $model_name \
+--test "../data2.2_more/${cur_lang}_${which_set}.conllu" --model_path "./model/" --extra_embed "../data2.2_more/wiki.multi.${cur_lang}.vec"
