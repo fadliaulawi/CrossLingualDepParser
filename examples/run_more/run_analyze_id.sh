d298b61a56058eb225f4dd92e43d80693523f043
fadlia68 <fadli.aulawi@ui.ac.id>
2022-09-20 11:45:39 +0000

    Successful rerun

--- examples/run_more/run_analyze_id.sh
+++ examples/run_more/run_analyze_id.sh
@@ -1,7 +1,7 @@
 model_name="network.pt"
-cur_lang="en"
+cur_lang="id"
 which_set="test"
 
-PYTHONPATH=../src/ CUDA_VISIBLE_DEVICES=$RGPU python2 ../src/examples/analyze.py --parser biaffine --ordered \
---punctuation 'PUNCT' 'SYM' --out_filename analyzer.$cur_lang.out --model_name $model_name \
---test "../data2.2_more/en_test.conllu" --model_path "./model/" --extra_embed "../data2.2_more/wiki.multi.${cur_lang}.vec"
+PYTHONPATH=../src/ CUDA_VISIBLE_DEVICES=1 python2 ../src/examples/analyze.py --parser biaffine --ordered \
+--punctuation 'PUNCT' 'SYM' --out_filename analyzer.$cur_lang.$which_set.out --model_name $model_name \
+--test "../data2.2_more/id_test.conllu" --model_path "./model/" --extra_embed "../data2.2_more/wiki.multi.id.vec"
