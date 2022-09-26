45f5aff93c76136560c6d07866f5daded3788cca
fadlia68 <fadli.aulawi@ui.ac.id>
2022-09-26 16:15:00 +0000

    Succes with javanese data only

--- examples/run_more/go_vocab.sh
+++ examples/run_more/go_vocab.sh
@@ -2,18 +2,17 @@
 
 # build
 embed_path=../data2.2_more/
-embed_path1=$embed_path/wiki.multi.en.vec
-embed_path2=$embed_path/wiki.multi.id.vec
+embed_path1=$embed_path/wiki.jv.vec
 data_path=../data2.2_more/
-data0=$data_path/en_train.conllu
-data1=$data_path/en_dev.conllu
-data2=$data_path/en_test.conllu
-data3=$data_path/id_train.conllu
-data4=$data_path/id_dev.conllu
-data5=$data_path/id_test.conllu
+data0=$data_path/jv_train.conllu
+data1=$data_path/jv_dev.conllu
+data2=$data_path/jv_test.conllu
+#data3=$data_path/id_train.conllu
+#data4=$data_path/id_dev.conllu
+#data5=$data_path/id_test.conllu
 
 PYTHONPATH=../src/ CUDA_VISIBLE_DEVICES= python2 ../src/examples/vocab/build_joint_vocab_embed.py \
---embed_paths $embed_path1 $embed_path2 \
---embed_lang_ids en id \
---data_paths $data0 $data1 $data2 $data3 $data4 $data5 \
+--embed_paths $embed_path1 \
+--embed_lang_ids jv \
+--data_paths $data0 $data1 $data2 \
 --model_path ./model/
--- examples/run_more/run_analyze_id.sh
+++
@@ -1,7 +0,0 @@
-model_name="network.pt"
-cur_lang="id"
-which_set="test"
-
-PYTHONPATH=../src/ CUDA_VISIBLE_DEVICES=1 python2 ../src/examples/analyze.py --parser biaffine --ordered \
---punctuation 'PUNCT' 'SYM' --out_filename analyzer.$cur_lang.$which_set.out --model_name $model_name \
---test "../data2.2_more/id_test.conllu" --model_path "./model/" --extra_embed "../data2.2_more/wiki.multi.id.vec"
