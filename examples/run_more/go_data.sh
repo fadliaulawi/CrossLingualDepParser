1cf7139211641bff22ac0f7afbe84c1b3a9f8f9f
Fadli Aulawi <77540198+fadlia68@users.noreply.github.com>
2022-09-26 09:53:53 +0700

    Update go_data.sh

--- examples/run_more/go_data.sh
+++ examples/run_more/go_data.sh
@@ -5,7 +5,7 @@ wget https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-4758/u
 tar -xzvf ud-treebanks-v2.10.tgz
 
 # data directory and codes
-git clone https://github.com/uclanlp/CrossLingualDepParser src
+git clone https://github.com/fadlia68/CrossLingualDepParser src
 mkdir -p ./data2.2_more/
 git clone https://github.com/Babylonpartners/fastText_multilingual data2.2_more/fastText_multilingual
 python3 ./src/examples/run_more/prepare_data.py |& grep -v "s$" | tee data2.2_more/log
