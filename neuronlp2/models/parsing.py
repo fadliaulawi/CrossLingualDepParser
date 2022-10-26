17b66f80b6855c6fafc38415671aee77f88d4162
fadlia68 <fadli.aulawi@ui.ac.id>
2022-10-26 01:28:57 +0000

    remove IO scenario

--- neuronlp2/models/parsing.py
+++ neuronlp2/models/parsing.py
@@ -126,24 +126,12 @@ class BiRecurrentConvBiAffine(nn.Module):
 	    print('input_word', type(input_word), input_word.size(), input_word)
             # [batch, length, word_dim]
             word = self.word_embedd(input_word)
-	    print('word1', type(word), word.size(), word)
+	    #print('word1', type(word), word.size(), word)
             # apply dropout on input
             word = self.dropout_in(word)
-	    print('word2', type(word), word.size(), word)
+	    #print('word2', type(word), word.size(), word)
 
             input = word
-		
-	    with open("../../embed.txt", encoding='utf-8', 'w') as f:
-		output_word = [','.join([str(s.data.numpy()[0]) for s in sentences]) for sentences in input_word]
-		f.write('\n'.join(output_word))
-	    
-	    os.system("python3 ../../examples/utility_bert.py --act_type embed")
-	    
-	    with open("../../embed_res.txt", encoding='utf-8') as f:
-		lines = f.readlines()
-		input_word = [[eval(l) for l in line.split(',')] for line in lines]
-	    
-	    
 
         if self.char:
             # [batch, length, char_length, char_dim]
