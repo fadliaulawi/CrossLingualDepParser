0861819b6d78ac60c3e2f81c7e9f06b8bfeefe44
fadlia68 <fadli.aulawi@ui.ac.id>
2022-11-15 16:10:06 +0000

    train test A_JV

--- examples/run_more/run_analyze_jv.sh
+++ examples/run_more/run_analyze_jv.sh
@@ -1,7 +1,7 @@
-model_name="network.pt"
+model_name="A_JV.pt"
 cur_lang="jv"
 which_set="test"
 
 PYTHONPATH=../src/ CUDA_VISIBLE_DEVICES=1 python3 ../src/examples/analyze.py --parser biaffine --ordered \
 --punctuation 'PUNCT' 'SYM' --out_filename analyzer.$cur_lang.$which_set.out --model_name $model_name \
---test "../data2.2_more/jv_test.conllu" --model_path "./model/" --extra_embed "../data2.2_more/wiki.jv.vec"
+--test "../data2.2_more/jv_test.conllu" --model_path "./model/" --extra_embed "../data2.2_more/wiki.jv.vec" --gpu
