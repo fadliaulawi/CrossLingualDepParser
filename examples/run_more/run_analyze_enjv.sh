1eb4dd4b3c3f7fb8f520654cf4343c7183e45818
Fadli Aulawi <fadli.aulawi@ui.ac.id>
2022-12-16 04:06:05 +0000

    add TL EN

--- examples/run_more/run_analyze_enjv.sh
+++ examples/run_more/run_analyze_enjv.sh
@@ -5,6 +5,6 @@ which_set="test"
 rm .env
 cp .envjv .env
 
-PYTHONPATH=../src/ CUDA_VISIBLE_DEVICES=2 python3 ../src/examples/analyze.py --parser biaffine --ordered \
+PYTHONPATH=../src/ CUDA_VISIBLE_DEVICES=1 python3 ../src/examples/analyze.py --parser biaffine --ordered \
 --punctuation 'PUNCT' 'SYM' --out_filename res/res_$code --model_name $model_name \
 --test "../data2.2_more/jv_test.conllu" --model_path "./model/" --extra_embed "../data2.2_more/wiki.jv.vec" --gpu
