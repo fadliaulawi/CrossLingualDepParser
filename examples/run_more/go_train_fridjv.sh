756500490a5b00b1192fc9c9d55bcff7189840f8
Fadli Aulawi <fadli.aulawi@ui.ac.id>
2022-12-18 16:01:24 +0000

    add HTL FR

--- examples/run_more/go_train_fridjv.sh
+++ examples/run_more/go_train_fridjv.sh
@@ -9,7 +9,7 @@ SEED=1234
 
 echo "Current seed is $SEED"
 
-PYTHONPATH=../src/ CUDA_VISIBLE_DEVICES=2 python3 ../src/examples/GraphParser.py \
+PYTHONPATH=../src/ CUDA_VISIBLE_DEVICES=1 python3 ../src/examples/GraphParser.py \
 --mode FastLSTM \
 --no_CoRNN \
 --hidden_size 300 \
